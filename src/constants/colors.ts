b9e3a73dbbb0f2e2a35b688483c648d537390084
Bel Ortega Casatejada <belen@dharma.nl>
2023-11-25 12:24:02 +0100

    Navbar and main content start

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const Primary = '#FE395C';
+export const Black = '#000000';
+export const DarkGrey = '#222222';
+export const LightGrey = '#918E9B';
+export const White = '#FFFFFF';
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Import the main SCSS file for styles
+import './scss/main.scss';
+
+// Import custom font files
+import './assets/fonts/AirbnbCereal_W_Bold.otf';
+import './assets/fonts/AirbnbCereal_W_Bold.otf';
+import './assets/fonts/AirbnbCereal_W_Bold.otf';
+
+// Import components
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
